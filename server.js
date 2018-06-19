f310940372e5a4014619713e770690dce65c25e3
tanat1994 <tanat1994@gmail.com>
2018-06-19 10:34:49 +0700

    fixed server.js

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const port = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// @dev render Inder.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 app.get('/api/video_list', (req, res) => {
   res.json(dummyData);
