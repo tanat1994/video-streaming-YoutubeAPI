cbcfdd42b238fc72c96f33cac74606d89403a932
tanat1994 <tanat1994@gmail.com>
2018-06-19 10:38:34 +0700

    fixed server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const path = require('path');
 const dummyData = require('./dummy-data/dummy');
 const app = express();
 const port = process.env.PORT || 5000;
