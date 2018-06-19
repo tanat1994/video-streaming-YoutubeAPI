4a7f81f46267434685067aab7219509284d1195a
tanat1994 <tanat1994@gmail.com>
2018-06-19 11:44:39 +0700

    fixed server

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-// @dev render Index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});
+// app.use(express.static(path.join(__dirname, 'client/build')));
+//
+// // @dev render Index.html
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/client/build/index.html'));
+// });
 
 app.get('/api/video_list', (req, res) => {
   res.send(dummyData);
