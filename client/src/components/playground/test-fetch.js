01277ed417d1d6c39581b3e705f8465575e13706
tanat1994 <tanat1994@gmail.com>
2018-06-19 11:13:04 +0700

    fixed server.js

--- client/src/components/playground/test-fetch.js
+++ client/src/components/playground/test-fetch.js
@@ -4,19 +4,22 @@ class Fetching extends Component {
   constructor () {
     super();
     this.state = {
-      videos: []
+      data: []
     };
   }
   componentDidMount () {
     fetch('/api/video_list')
       .then(res => res.json())
-      .then(videos => this.setState({ videos }, () => console.log('Video fetched ... ', videos)));
+      .then(data => this.setState({ data }, () => console.log('Video fetched ... ', data)));
   }
   render () {
     return (
       <div>
-        {this.state.videos.map((video) => (
-            <h1>{ video.id }</h1>
+        <h1> Test fetched </h1>
+        {this.state.data.map((data) => (
+            <li key={ data.id }>
+              { data.text }
+            </li>
           )
         )}
       </div>
