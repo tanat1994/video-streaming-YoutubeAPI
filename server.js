7035aa32921f6790c486923641aed8a8262b067a
tanat1994 <tanat1994@gmail.com>
2018-06-19 10:38:14 +0700

    fixed server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 
 const dummyData = require('./dummy-data/dummy');
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
